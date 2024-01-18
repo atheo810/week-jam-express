cb6367fc77dc3004b130ac985684f126919c0b86
atheo810 <atheos810@gmail.com>
2024-01-18 08:39:50 +0800

    feat: fix response

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,6 +38,7 @@ class bookController {
           category: req.body.category,
         },
       });
+      res.status(201).json(result);
     } catch (error) {
       res.status(400).json({ message: error });
     }
